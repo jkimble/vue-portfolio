bf648210e43432b4adf2bedb33036d795184634b
Justin <jkimble.dev@gmail.com>
2023-06-20 18:30:24 -0400

    change urls

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/jkimble-portfolio/',
+  base: '/vue-portfolio/',
   plugins: [
     vue(),
     vueJsx(),
